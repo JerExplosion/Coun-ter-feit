4081d6d48a6b3ba464eb288fc390baeb203ad4f6
jer .explosion <jerryren.ios@gmail.com>
2021-05-31 23:50:36 -0400

    constantsforbang .init

---
+++ Bang/ConstantsForBang.swift
@@ -0,0 +1,20 @@
+//
+//  ConstantsForBang.swift
+//  Bang
+//
+//  Created by Jerry Ren on 5/9/21.
+//
+
+struct ConstantsForBang {
+    struct Wallpapers {
+        static let cero = "iphone_wallpaper_cero"
+        static let uno = "iphone_wallpaper_uno"
+        static let dos = "iphone_wallpaper_dos"
+        static let tres = "iphone_wallpaper_tres"
+        static let cuatro = "iphone_wallpaper_cuatro"
+        static let cinco = "iphone_wallpaper_cinco"      
+    }
+    struct Shu {
+        
+    }
+}
